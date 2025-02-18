ef7ed3a7a32ae8ef71c6572ef6f906f05de7ecc6
Alathea FERRAND <alatheaferrand@gmail.com>
2025-02-18 08:45:39 -1000

    Suite de Fibonacci

---
+++ eau03.rb
@@ -0,0 +1,35 @@
+# Suite de Fibonacci
+# Affiche le N-ème élément de la célèbre suite de Fibonacci
+# (0, 1, 1, 2) étant le début de la suite
+# et le premier élément état à l'index 0
+# Affiche -1 si le paramètre est négatif ou mauvais
+
+# Fonction
+def fibonacci(index)
+  index = index.to_i
+  return index if index < 2 # car index 0 > resultat 0 et index 1 > resultat 1
+
+  prev = 0
+  curr = 1
+  i = 2
+  
+  while i <= index
+    prev, curr = curr, prev + curr
+    i += 1
+  end
+  return curr
+end
+
+# Gestion d'erreur
+if ARGV.length != 1 || ARGV[0] !~ (/\A\d+\z/) || ARGV[0].to_i < 0
+  puts '-1'
+  exit
+end
+
+# Parsing
+
+# Resolution
+resultat = fibonacci(ARGV[0])
+
+# Resultat
+puts resultat
