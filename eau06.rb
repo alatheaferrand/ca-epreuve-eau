a3e01e292d69dea3793c66fb0f3182a3058b5f7f
Alathea FERRAND <alatheaferrand@gmail.com>
2025-02-28 19:06:28 -1000

    [Majuscule sur deux] refactor: improve structure and ensure string mutability

--- eau06.rb
+++ eau06.rb
@@ -1,77 +1,56 @@
+# frozen_string_literal: true
+
 # Majuscule sur deux
 # Affiche une lettre sur 2 en majuscule
-# Seule les lettres (A-Z, a-z) sont prises en compte
-# Affiche error et quitter le programme en cas de problèmes d'arguments
+# Seules les lettres (A-Z, a-z) sont prises en compte
+# Affiche error et quitte le programme en cas de problème d'arguments.
 
-# String dans string
-# Détermine si une chaîne de caractères est contenu dans une autre chaîne
-# Affiche error et quitter le programme en cas de problèmes d'arguments
+# ========================
+# Utility Functions
+# ========================
+def alternate_case_text(text)
+  result = String.new
+  processed_letters = 0
 
-# Fonction
-def contient(string)
-    longueur = string.length
-
-    # Alphabets majuscules et minuscules
-    majuscules = ('A'..'Z').to_a
-    minuscules = ('a'..'z').to_a
-  
-    # Parcours string
-    i = 0
-    result = ""
-    position_lettre = 0 # Compteur qui ignore les caractères qui ne sont pas des lettres
-
-     while i < longueur
-      lettre = false # Flag pour savoir si on a rencontré une lettre
-
-      # Vérification si minuscule
-      for n in (0...minuscules.length)
-        if minuscules[n] == string[i]
-          if position_lettre % 2 == 0
-            result << majuscules[n]
-          else
-            result << minuscules[n]
-          end
-          position_lettre += 1
-          lettre = true
-          break
-        end
-      end
-
-      # Vérification si majuscule
-      for n in (0...majuscules.length)
-        if majuscules[n] == string[i]
-          if position_lettre % 2 != 0
-            result << minuscules[n]
-          else
-            result << majuscules[n]
-          end
-          position_lettre += 1
-          lettre = true
-          break
-        end
-      end
-
-      # Si ce n'est pas une lettre (lettre = false), on l'affiche tel quel
-      result << string[i] if !lettre
-
-      i += 1
+  text.each_char do |char|
+    if char.between?('A', 'Z') || char.between?('a', 'z')
+      char = processed_letters.even? ? char.upcase : char.downcase
+      processed_letters += 1
     end
-  
-    return result
+    result << char
   end
-  
-  # Gestion d'erreur
-  if ARGV.length != 1 || ARGV[0] =~ (/\A\d*.\d*\z/)
-    puts 'error'
-    exit
-  end
-  
-  # Parsing
-  string = ARGV[0]
-  
-  # Resolution
-  resultat = contient(string)
-  
-  # Resultat
-  puts resultat
-  
+
+  result
+end
+
+# ========================
+# Error Handling
+# ========================
+def validate_text_argument(args)
+  return if args.size == 1 && args[0].count('a-zA-Z').positive?
+
+  puts 'error'
+  exit
+end
+
+# ========================
+# Parsing Arguments
+# ========================
+def extract_valid_text_argument
+  args = ARGV
+  validate_text_argument(args)
+  args[0]
+end
+
+# ========================
+# Problem Solving
+# ========================
+def apply_alternate_case
+  text = extract_valid_text_argument
+  alternate_case_text(text)
+end
+
+# ========================
+# Execution
+# ========================
+puts apply_alternate_case
