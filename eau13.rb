02793ab10bc19e003ef0683f20e4ce94fbd07522
Alathea FERRAND <alatheaferrand@gmail.com>
2025-03-02 16:06:49 -1000

    [Tri par sélection] refactor: improve selection sort implementation

--- eau13.rb
+++ eau13.rb
@@ -1,77 +1,75 @@
+# frozen_string_literal: true
+
 # Tri par sélection
-# Trie une liste de nombres
-# Implémente l'algorithme du tri par sélectoin
+# Trie une liste de nombres.
+# Implémente l'algorithme du tri par sélection.
 # Utiliser une fonction de cette forme :
 # my_selection_sort(array) {
 #   votre algorithme
-#  return (new_array)
+#   return (new_array)
 # }
-# Afficher error et quitter le programme en cas de problèmes d'arguments
+# Afficher error et quitter le programme en cas de problèmes d'arguments.
 
-# Fonctions
-def convert_to_nombres(tableau)
-  # Vérifier que les éléments du tableau sont des nombres et les convertir en nombres
-  nombres = []
-  for i in (0...tableau.length)
-    unless tableau[i] =~ /\A[-+]?\d+\z/
-      puts 'error'
-      exit
-    end
-    nombres << tableau[i].to_i
-  end
-  return nombres
+# ========================
+# Utility Functions
+# ========================
+
+def numeric?(value)
+  !Integer(value, exception: false).nil?
 end
-  
+
+# Implémentation du tri par sélection
 def my_selection_sort(array)
-  i = 0
-  while i < array.length - 1
+  length = array.length
+
+  (0...length - 1).each do |i|
     min_index = i
-    j = i + 1
 
-    while j < array.length
-      if array[j] < array[min_index] # Trouver l'index du plus petit élément
-        min_index = j
-      end
-      j += 1
+    (i + 1...length).each do |j|
+      min_index = j if array[j] < array[min_index]
     end
 
-    # Echanger l'élément trouvé avec l'élément à la position actuelle
-    if min_index != i
-      temporaire = array[i]
-      array[i] = array[min_index]
-      array[min_index] = temporaire
-    end
-
-    i += 1
+    array[i], array[min_index] = array[min_index], array[i] if min_index != i
   end
-  return array
+
+  array
 end
 
-def array_to_string(new_array)
-  i = 0
-  string = ""
-  while i < new_array.length
-    string << new_array[i].to_s
-    string << ' ' if i < new_array.length - 1
-    i += 1
-  end
-  return string
-end
-  
-# Gestion d'erreur
-if ARGV.empty? || ARGV.length < 2
-puts 'error'
-exit
+# ========================
+# Error Handling
+# ========================
+
+def validate_numeric_arguments(args)
+  return if args.size >= 2 && args.all? { |arg| numeric?(arg) }
+
+  puts 'error'
+  exit
 end
 
-# Parsing
-tableau = ARGV
+# ========================
+# Parsing Arguments
+# ========================
 
-# Resolution
-array = convert_to_nombres(tableau)
-new_array = my_selection_sort(array)
-resultat = array_to_string(new_array)
+def extract_numbers
+  args = ARGV
+  validate_numeric_arguments(args)
 
-# Resultat
-puts resultat
-  
+  numbers = []
+  args.each { |arg| numbers << arg.to_i }
+  numbers
+end
+
+# ========================
+# Problem Solving
+# ========================
+
+def compute_sorted_numbers
+  numbers = extract_numbers
+  my_selection_sort(numbers).join(' ')
+end
+
+# ========================
+# Execution
+# ========================
+
+puts compute_sorted_numbers
